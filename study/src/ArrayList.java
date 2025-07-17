bc6bec1e71f4087f665c870f5f19a9fec931cbf3
kht <gudxor1541@naver.com>
2025-07-17 20:02:34 +0900

    250717스터디

--- study/src/ArrayList.java
+++ study/src/ArrayList.java
@@ -38,22 +38,53 @@ public class ArrayList {
         if (index == array.length) return true;
         else return false;
     }
-//
-//    ? ? get(?){
-//        ?
-//    }
-//
-//    ? ? clear(?){
-//        ?
-//    }
-//
-//
-//    ? ? isEmpty(?){
-//        ?
-//    }
-//
-//    ? ? remove(?){
-//        ?
-//    }
+
+    public int get(int index){
+        if(this.index <= index){
+            throw new ArrayIndexOutOfBoundsException("index out of bounds");
+        }
+        return array[index];
+    }
+
+    public void clear(){
+        array = null;
+        index = 0;
+    }
+
+
+    public boolean isEmpty(){
+        if(isExists()) return true;
+        else if(index == 0) return true;
+        else return false;
+    }
+
+
+//   0 1 2 3 4 5 6 7 8 9 0 0 0
+//   0 1 2 3 4 5 6 7 9 9 0 0 0
+    public int remove(int index){
+        int removeValue = array[index];
+        for(int i = index; i< this.index-1; i++){
+            array[i] = array[i+1];
+        }
+
+        this.index--;
+        return removeValue;
+    }
+    // 1 2 3 4 5 0 0 0 0 0
+    public void trimToSize(){
+        if(!isExists() || index == array.length) return;
+
+        int[] newArr = new int[index];
+        for(int i = 0; i<index; i++){
+            newArr[i] = array[i];
+        }
+        array = newArr;
+
+    }
+
+    @Override
+    public String toString() {
+        return array.toString();
+    }
 
 }
--- study/src/Main.java
+++ study/src/Main.java
@@ -1,15 +1,19 @@
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
-        list.add("a");
-        list.get(100);
-//        list.clear();
-//        list.size();
-//        list.isEmpty();
-//        list.remove(0);
-//        list.trimToSize();
+        ArrayList list = new ArrayList();
+
+//        System.out.println(list);
+        list.add(1);
+        System.out.println(list);
+        list.add(1);list.add(1);list.add(1);list.add(1);
+        list.add(1);list.add(1);list.add(1);list.add(1);
+        list.add(1);list.add(1);
+        System.out.println(list);
+        list.trimToSize();
+        System.out.println(list);
+
+
     }
 }
